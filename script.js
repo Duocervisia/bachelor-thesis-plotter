8cd3fa91462b5637bfc1adbc376ac8b437daa8ca
Tobias Henning <henningt@htw-berlin.de>
2024-03-26 11:17:22 +0100

    Beyer setup

--- script.js
+++ script.js
@@ -18,13 +18,22 @@ loadData = function(url) {
 let ch = loadData("weather.json");
 let euler = new Euler();
 let resultsEuler = euler.run(ch);
+let beyer = new Beyer();
+let resultsBeyer = beyer.run(ch);
+let beyerNew = new BeyerNew();
+let resultsBeyerNew = beyerNew.run(ch);
 ch = loadData("weather1min.json");
 let eulerOneMin = new EulerOneMin();
 let resultsEulerOneMin = eulerOneMin.run(ch);
 
+
 let newData = [];
+let beginnTime = 1504173600;
+let endTime = 1504188000;
+// let beginnTime = 1501480800;
+// let endTime = 1501509600;
 resultsEulerOneMin.forEach((d) => {
-    if(d[0] <= 1504188000 && d[0] >= 1504173600) {
+    if(d[0] <= endTime && d[0] >= beginnTime) {
         newData.push({
             date: new Date(d[0] * 1000),
             moduleTemperatureEulerOneMin: d[2],
@@ -33,7 +42,7 @@ resultsEulerOneMin.forEach((d) => {
     }
 });
 resultsEuler.forEach((d) => {
-    if(d[0] <= 1504188000 && d[0] >= 1504173600) {
+    if(d[0] <= endTime && d[0] >= beginnTime) {
         //find the corresponding data point in newData, where the date is the same
         let index = newData.findIndex((element) => {
             return element.date.getTime() === d[0] * 1000;
@@ -44,6 +53,56 @@ resultsEuler.forEach((d) => {
         }
     }
 });
+resultsBeyer.forEach((d) => {
+    if(d[0] <= endTime && d[0] >= beginnTime) {
+        //find the corresponding data point in newData, where the date is the same
+        let index = newData.findIndex((element) => {
+            return element.date.getTime() === d[0] * 1000;
+        });
+
+        if(index !== -1) {
+            newData[index].moduleTemperatureBeyerFiveMin = d[1];
+        }
+    }
+});
+resultsBeyerNew.forEach((d) => {
+    if(d[0] <= endTime && d[0] >= beginnTime) {
+        //find the corresponding data point in newData, where the date is the same
+        let index = newData.findIndex((element) => {
+            return element.date.getTime() === d[0] * 1000;
+        });
+
+        if(index !== -1) {
+            newData[index].moduleTemperatureBeyerNewFiveMin = d[1];
+        }
+    }
+});
+let totalDiffEuler = 0;
+let totalDiffBeyer = 0;
+let totalDiffBeyerNew = 0;
+
+newData.forEach((d) => {
+    if(d.moduleTemperatureEulerFiveMin === undefined) {
+        return;
+    }
+    totalDiffEuler += Math.abs(d.moduleTemperatureEulerOneMin - d.moduleTemperatureEulerFiveMin);
+    totalDiffBeyer += Math.abs(d.moduleTemperatureEulerOneMin - d.moduleTemperatureBeyerFiveMin);
+    totalDiffBeyerNew += Math.abs(d.moduleTemperatureEulerOneMin - d.moduleTemperatureBeyerNewFiveMin);
+});
+console.log("Total difference Euler: " + totalDiffEuler);
+console.log("Total difference Beyer: " + totalDiffBeyer);
+console.log("Total difference Beyer New: " + totalDiffBeyerNew);
+let legendDiv = document.getElementById("legend");
+let span = document.createElement("span");
+span.textContent = totalDiffEuler.toFixed(2) + " Euler Abweichung";
+legendDiv.appendChild(span);
+span = document.createElement("span");
+span.textContent = totalDiffBeyer.toFixed(2) + " Beyer Abweichung";
+legendDiv.appendChild(span);
+span = document.createElement("span");
+span.textContent = totalDiffBeyerNew.toFixed(2) + " Beyer New Abweichung";
+legendDiv.appendChild(span);
+
 const myChart = new Chart(document.getElementById("myChart"), {
     type: "line",
     data: {
@@ -65,9 +124,23 @@ const myChart = new Chart(document.getElementById("myChart"), {
                 fill: false,
             },
             {
-                label: "Module Temperature Euler",
+                label: "Module Temperature Euler 5 min",
                 data: newData.map((d) => d.moduleTemperatureEulerFiveMin),
                 borderColor: "green",
+                fill: false,
+                showLine: true, // This will show the line
+            },
+            {
+                label: "Module Temperature Beyer 5 Min",
+                data: newData.map((d) => d.moduleTemperatureBeyerFiveMin),
+                borderColor: "purple",
+                fill: false,
+                showLine: true, // This will show the line
+            },
+            {
+                label: "Module Temperature Beyer New 5 Min",
+                data: newData.map((d) => d.moduleTemperatureBeyerNewFiveMin),
+                borderColor: "red",
                 fill: false,
                 showLine: true, // This will show the line
             },
